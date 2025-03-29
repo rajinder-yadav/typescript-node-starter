5e35f6ad5594077c8015cddf8f9966ffcc40ed92
Dev Guy <devguy.ca@gmail.com>
2025-03-28 23:43:18 -0400

    config: fix

--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,7 @@ export const message: string = "Hello World!";
 
 export function showMessage(message: string, count: number) {
   for (let i = 0; i < count; i++) {
-    console.log(`${i}) message`);
+    console.log(`${i}) ${message}`);
   }
 
 }
